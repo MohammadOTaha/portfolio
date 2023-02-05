0d09e4690043099c6c2aec302b46763a74d69f10
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-02-05 03:30:28 +0200

    chore: add prettier and eslint configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
