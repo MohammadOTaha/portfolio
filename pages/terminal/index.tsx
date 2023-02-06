1083e43921cea8ff1cc059529602b403323f3452
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-02-06 18:55:11 +0200

    feat: change home page to terminal

--- pages/terminal/index.tsx
+++ pages/terminal/index.tsx
@@ -7,9 +7,7 @@ export default function TerminalPage() {
       flex-col
       gap-8
       items-center
-      h-screen
-      bg-gray-100'
-    >
+      '>
       <div className='
         flex
         flex-col
@@ -20,18 +18,22 @@ export default function TerminalPage() {
         mt-20
       '>
         {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <span className='text-gray-700 font-bold'>Welcome!</span>
-        <span className='text-gray-500 mt-2 font-bold'>Use the Terminal to know more about me...</span>
+        <span className='text-gray-800 font-bold'>Welcome!</span>
+        <span className='text-gray-500 mt-2 font-bold'>Use the
+          <span className='text-gray-700 text-transparent bg-clip-text bg-gradient-to-r from-amber-300 via-fuchsia-400 to-violet-400'> Terminal </span>
+          to know more about me
+        </span>
       </div>
 
       <Terminal />
 
       <div
-        className='fixed top-2/5 right-1/3 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob' />
+        className='fixed top-1/2 right-1/2 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob' />
       <div
-        className='fixed top-1/2 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000' />
+        className='fixed bottom-96 w-80 h-80 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000' />
       <div
-        className='fixed top-1/5 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000' />
+        className='fixed top-96 right-1/3 w-80 h-80 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000' />
+
     </div>
   );
 }
