5a9f7e6ef7e3743e867bbcf2d2553b4098a52fe3
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-02-06 19:34:28 +0200

    fix: home page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import TerminalPage from './terminal';
+import Head from 'next/head';
 
 export default function Home() {
-  return <TerminalPage />;
-}
+  return <>
+    <Head>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
+      <title>Omar's Portfolio</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <TerminalPage />
+  </>
+};
