cae24a10c149559b6265b234f1e9cbdb33f33bbf
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-02-07 04:54:24 +0200

    fix: empty command output

--- services/ExecuteCommand.ts
+++ services/ExecuteCommand.ts
@@ -1,6 +1,10 @@
 import CommandsOutputs from '../public/commands.json';
 
 export default function executeCommand(command: string): string {
+  if (command === '') {
+    return '';
+  }
+
   if (CommandsOutputs[command as keyof typeof CommandsOutputs]) {
     return CommandsOutputs[command as keyof typeof CommandsOutputs];
   }
